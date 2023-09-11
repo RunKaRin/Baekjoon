e7c890234a4d01773556ae09e7d8608f0ccc1683
이건주 <137998128+RunKaRin@users.noreply.github.com>
2023-09-11 20:57:26 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        word = word.replaceAll("c=", " ");
+        word = word.replaceAll("c-", " ");
+        word = word.replaceAll("dz=", " ");
+        word = word.replaceAll("d-", " ");
+        word = word.replaceAll("lj", " ");
+        word = word.replaceAll("nj", " ");
+        word = word.replaceAll("s=", " ");
+        word = word.replaceAll("z=", " ");
+        System.out.println(word.length());
+        sc.close();
+    }
+}
