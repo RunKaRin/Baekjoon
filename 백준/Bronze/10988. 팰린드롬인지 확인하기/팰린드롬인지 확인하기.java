11f576827ee42e764b33e34814ba9e65f639b8f1
이건주 <137998128+RunKaRin@users.noreply.github.com>
2023-09-13 09:20:05 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 204 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main2 { // 백준 10988번
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String word = sc.next();
-        char [] array = word.toCharArray();
+        String word = sc.next();                // 입력문자 배열
+        char [] array = word.toCharArray();     // 입력문자 역배열
         char [] reverse = new char[array.length];
         int a = 0;
 
         for (int i = 0; i < array.length; i++) {
-            reverse[array.length-1-i] = array[i];
+            reverse[array.length-1-i] = array[i];   // 역배열 완성
         }
+//        // ↓↓ 콘솔 확인용
+//        for (int b = 0; b < array.length; b++) {
+//            System.out.print(array[b]);
+//        }
+//        System.out.println();
+//        for (int c = 0; c < reverse.length; c++) {
+//            System.out.print(reverse[c]);
+//        }
+//        System.out.println();
+//        // ↑↑ 콘솔 확인용
         for (int i = 0; i < array.length; i++) {
             if (array[i] != reverse[i]) {
-                a++; // 팰린드롬이 아니면 1씩 증가
+                a++; break;
             }
         }
         if (a == 0) {
