7f10dc3293a366aefdae132ace03731a840abe02
이건주 <137998128+RunKaRin@users.noreply.github.com>
2023-09-14 11:15:51 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 592 ms, Memory: 24072 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N-1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
+            for (int kk = 0; kk < i; kk++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < N-1; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int  k = i; k < N-1; k++) {
+                System.out.print("*");
+            }
+            for (int kk = i; kk < N-2; kk++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
