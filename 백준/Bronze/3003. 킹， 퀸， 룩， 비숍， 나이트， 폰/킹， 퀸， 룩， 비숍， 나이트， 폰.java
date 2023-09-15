d85cd48edcde451be3eb0e3cd23ef8a4c5e6f152
이건주 <137998128+RunKaRin@users.noreply.github.com>
2023-09-15 23:37:32 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 240 ms, Memory: 18344 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] chess = {1, 1, 2, 2, 2, 8};
+        int[] input = new int[6];
+        int[] output = new int[6];
+        for (int i = 0; i < 6; i++) {
+            input[i] = sc.nextInt();
+            output[i] = chess[i] - input[i];
+        }
+        for (int i = 0; i < 6; i ++) {
+            System.out.print(output[i] + " ");
+        }
+        sc.close();
+    }
+}
