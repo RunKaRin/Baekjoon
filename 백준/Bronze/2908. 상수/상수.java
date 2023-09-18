6fcc446877f570813ef13ec540d0dd9507df0f18
이건주 <137998128+RunKaRin@users.noreply.github.com>
2023-09-18 23:38:29 +0900

    [Bronze II] Title: 상수, Time: 212 ms, Memory: 17492 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String num1 = sc.next();
+        String num2 = sc.next();
+
+
+        char[] array1 = num1.toCharArray();
+        for (int i = 0; i<1; i++) {
+            char x = array1[0];
+            array1[0] = array1[2];
+            array1[2] = x;
+        }
+        char[] array2 = num2.toCharArray();
+        for (int i = 0; i<1; i++) {
+            char x = array2[0];
+            array2[0] = array2[2];
+            array2[2] = x;
+        }
+        if ((int)array1[0] > (int)array2[0]) {
+            System.out.print(array1);
+        } else if ((int)array1[0] < (int)array2[0]) {
+            System.out.print(array2);
+        } else if ((int)array1[0] == (int)array2[0]) {
+
+            if ((int) array1[1] > (int) array2[1]) {
+                System.out.print(array1);
+            } else if ((int) array1[1] < (int) array2[1]) {
+                System.out.print(array2);
+            } else if ((int)array1[1] == (int)array2[1]) {
+
+                if ((int) array1[2] > (int) array2[2]) {
+                    System.out.print(array1);
+                } else  if ((int) array1[2] < (int) array2[2]) {
+                    System.out.print(array2);
+                } else {
+                    System.out.println(array1);
+                }
+            }
+        }
+    }
+}
